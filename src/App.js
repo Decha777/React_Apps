ecf49121c99f72f222ce43644bf22ea1c8d056cc
Decha777 <Decha777d@gmail.com>
2021-01-12 17:02:19 +0300

    header test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa';
+import logo from './logo.png';
 
+import styled from 'styled-components'
+
+const MyHeader = styled.div`
+display :flex;
+justify-content: space-between;
+align-items: center;
+padding: 1rem;
+border-bottom: solid .1px black;
+
+img{
+  width: 4rem;
+  height: 4rem;
+}
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>On Build by Decha777</h1>
-    </div>
+    <MyHeader>
+      <img src={logo} alt="logo" />
+      <FaBars />
+    </MyHeader>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyles } from './globalSytles'
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,4 +22,4 @@ reportWebVitals();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
